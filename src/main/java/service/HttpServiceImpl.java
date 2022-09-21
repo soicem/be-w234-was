62de0768eb291c4bd4380ae43807d5c29b9d6c0d
dwayne.knq <dwayne.knq@kakaocorp.com>
2022-09-21 19:51:29 +0900

    bug: 사용자 입력 시 User 추가하도록 변경

--- src/main/java/service/HttpServiceImpl.java
+++ src/main/java/service/HttpServiceImpl.java
@@ -18,9 +18,8 @@ public class HttpServiceImpl implements HttpService {
             BufferedReader br = null;
             br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
             String path = httpRequestUtils.extractPath(br.readLine());
-
             Map<String, String> m = httpRequestUtils.parseQueryString(path);
-            userRepository.add(new User(m.get("userId"), m.get("password"), m.get("name"), m.get("email")));
+            if(m.size() != 0) userRepository.add(new User(m.get("userId"), m.get("password"), m.get("name"), m.get("email")));
             return path;
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -30,7 +29,6 @@ public class HttpServiceImpl implements HttpService {
     @Override
     public byte[] responseHTML(String path) {
         try {
-            HttpRequestUtils httpRequestUtils = new HttpRequestUtils();
             return httpRequestUtils.makeBody(path);
         } catch (IOException e) {
             throw new RuntimeException(e);
